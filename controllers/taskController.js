d21a5bd1123bce65dbf1601d3c0efee97ebd6f79
aishwaryambbug <aishwaryamb077@gmail.com>
2026-05-01 19:36:24 +0530

    task 1 creation

---
+++ controllers/taskController.js
@@ -0,0 +1,21 @@
+const Task = require("../models/Task");
+
+// Create task
+exports.createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Get tasks
+exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors()); // allow frontend connection
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
+// 👉 ADD THIS LINE (VERY IMPORTANT)
+app.use("/api/tasks", require("./routes/taskRoutes"));
 
 // Test route
 app.get("/", (req, res) => {
